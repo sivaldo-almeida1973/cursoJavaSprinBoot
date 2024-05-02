b9237c2c0779167bff8b4beaddf09601719a4961
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-05-02 20:53:04 -0300

    H2 database

--- src/main/java/com/devsuperior/myfirstproject/entities/Category.java
+++ src/main/java/com/devsuperior/myfirstproject/entities/Category.java
@@ -7,17 +7,24 @@ import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Category implements Serializable{
 	
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String name;
 	
-	
-	
-	
-	@JsonIgnore                   //lista de produtos
+	@JsonIgnore    
+	@OneToMany(mappedBy = "category")                      //lista de produtos
 	private List<Product> products = new ArrayList<>();
 	
 	public Category() {
